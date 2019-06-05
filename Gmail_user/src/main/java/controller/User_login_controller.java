f3884cefc8b0bf8bee7db818d12752ce15341381
mortezaKazemi <azetrom.kazemi2000@gmail.com>
2019-06-05 16:59:10 +0430

    Maven and git initialized MVC template created

---
+++ Gmail_user/src/main/java/controller/User_login_controller.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+public class User_login_controller extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
